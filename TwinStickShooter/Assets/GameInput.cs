d0beaa789123b4cd369df87ae29da98f91b85938
Roberto Frías Bernal <roberfrias115@gmail.com>
2015-02-02 16:51:00 +0100

    Comienzo
    Movimiento personaje

---
+++ TwinStickShooter/Assets/GameInput.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameInput : MonoBehaviour {
+
+	static float ejeX;
+	static float ejeY;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ejeX = Input.GetAxis ("Horizontal");
+		ejeY = Input.GetAxis ("Vertical");
+	}
+}
---
+++ TwinStickShooter/Assets/PlayerShip.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShip : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rigidbody2D.velocity = GameInput.ejeX * Vector2.right + GameInput.ejeY * Vector2.up;
+		rigidbody2D.velocity.Normalize();
+		rigidbody2D.velocity = rigidbody2D.velocity * maxSpeed;
+	}
+}
