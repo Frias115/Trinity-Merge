37a05e08e92799d2d5f7ccba705fe4c3ad9c17da
Sergio Roselló Morell <sergio-rosello@hotmail.com>
2015-03-09 22:11:13 +0100

    Fondo de escritorio.
    He anadido unos efectos de fondo, me gustaria sabes si os gusta o no.
    yo creo que no acaba de salir bien.
    Lo he hecho porque el profe me lo aconsejo de camino a casa.

---
+++ TwinStickShooter/Assets/BackgroundAnimations.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundAnimations : MonoBehaviour {
+	public int rotationSpeed;
+	public int scalationSpeed;
+	public int translationSpeed;
+	public AnimationCurve rotation;
+	public AnimationCurve scalation;
+	public AnimationCurve translation;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector2.right * Time.deltaTime*rotationSpeed);
+	}
+}
