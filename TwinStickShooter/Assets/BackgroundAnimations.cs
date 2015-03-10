e998651ae139cd32fdd279c316f36fbea12c0d66
Sergio Roselló Morell <sergio-rosello@hotmail.com>
2015-03-10 19:56:58 +0100

    Reescalado del fondo del juego
    Ej jugador sigue experimentando el mismo bug.
    Implementado otro comportamiento del fondo, pero es algo experimental.
    Aun no esta bien para implementar.

--- TwinStickShooter/Assets/BackgroundAnimations.cs
+++ TwinStickShooter/Assets/BackgroundAnimations.cs
@@ -2,19 +2,26 @@
 using System.Collections;
 
 public class BackgroundAnimations : MonoBehaviour {
+	private float contador;
+	public int intervalo;
 	public int rotationSpeed;
 	public int scalationSpeed;
 	public int translationSpeed;
+	public float verticalSpeed;
 	public AnimationCurve rotation;
 	public AnimationCurve scalation;
 	public AnimationCurve translation;
 	// Use this for initialization
 	void Start () {
-
+		contador += Time.deltaTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate (Vector2.right * Time.deltaTime*rotationSpeed);
+		//transform.position += -Vector3.up * Time.deltaTime * verticalSpeed;
 	}
 }
+
+
+
