530da7f3df0c0e75440b42f6719c090621c994f5
Roberto Frías Bernal <roberfrias115@gmail.com>
2015-03-12 11:31:20 +0100

    Correccion error spawn

--- TwinStickShooter/Assets/WaveController.cs
+++ TwinStickShooter/Assets/WaveController.cs
@@ -4,13 +4,15 @@ using System.Collections;
 public class WaveController : MonoBehaviour {
 
 	public GameObject[] waves;
+	GameObject activeWave;
 	int numberWave = 0;
 	static float spawnTimer = 0;
 	public float waitTime = 5;
 
 	// Use this for initialization
 	void Start () {
-		waves [numberWave].SetActive (true);
+		activeWave = Instantiate (waves[numberWave]) as GameObject;
+		activeWave.SetActive (true);
 	}
 	
 	// Update is called once per frame
@@ -18,9 +20,14 @@ public class WaveController : MonoBehaviour {
 		if (SpawnController.enemigosRestantes ==  0 && numberWave < waves.Length) {
 			spawnTimer +=  Time.deltaTime;
 			if(spawnTimer > waitTime){
-				waves [numberWave].SetActive (false);
+				Destroy (activeWave);
+				//waves [numberWave].SetActive (false);
 				numberWave++;
-				waves [numberWave].SetActive (true);
+				Debug.Log(numberWave);
+				activeWave = Instantiate (waves[numberWave]) as GameObject;
+				Debug.Log(activeWave);
+				activeWave.SetActive (true);
+				//waves [numberWave].SetActive (true);
 				spawnTimer = 0;
 			}
 			
