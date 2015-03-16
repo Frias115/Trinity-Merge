48af07caaa78aff8bfbf6b9738ee5f34166cc120
Dielex <dielex117@gmail.com>
2015-03-16 16:19:33 +0100

    Final relativo
    Algunas partículas y otras cosejas

--- TwinStickShooter/Assets/scripts/runner.cs
+++ TwinStickShooter/Assets/scripts/runner.cs
@@ -11,28 +11,4 @@ public class runner : EnemyShip {
 		transform.rotation = Quaternion.Slerp (transform.rotation,rot, rotationSpeed*Time.deltaTime);
 		base.Move ();
 	}
-	/*
-	public override void Move () {
-		float rushFrontal, rushLateral;
-		bool rush;
-		//rigidbody2D.velocity = Vector2.up *frontalCurve.Evaluate(timer/frontalLoop) *velocidadFrontal + Vector2.right *lateralCurve.Evaluate(timer/lateralLoop) *velocidadLateral;
-		if(!rush) {
-			rigidbody2D.velocity = Vector2.up * velocidadFrontal + Vector2.right * velocidadLateral;
-			if(enemyPositionY == PlayerShip.playerPositionY || enemyPositionX == PlayerShip.playerPositionX)
-			{
-				rush = true;
-			}
-		}
-		else
-		{
-			if (enemyPositionX < PlayerShip.playerPositionX) 
-			{
-				rigidbody2D.velocity = Vector2.right * rushLateral;
-			}
-			else if (enemyPositionX > PlayerShip.playerPositionX)
-			{
-				rigidbody2D.velocity = Vector2.right * -rushLateral;
-			}
-		}
-	}*/
 }
