89950e67c2e47a1d4c75b729b965d45405adc67a
Sergio Roselló Morell <sergio-rosello@hotmail.com>
2015-03-16 20:28:21 +0100

    Primer mapa hecho
    Se ha hecho el primer mapa.
    Consiste del fondo negro y los cubos crecen, se encojen y cambian de
    color. cada 0.33 segundos.

--- TwinStickShooter/Assets/changeColor.cs
+++ TwinStickShooter/Assets/changeColor.cs
@@ -3,20 +3,24 @@ using System.Collections;
 
 public class changeColor : MonoBehaviour {
 
-
+	float nextUsage;
 	public Color[] colors;
 	public float tiempoDeCadaColor;
 	// Use this for initialization
 	void Start () {
-	
+		nextUsage = Time.time + tiempoDeCadaColor;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		gameObject.renderer.material.color = colors [Random.Range (0, colors.Length)];
+
+		if(Time.time > nextUsage)
+		{
+			nextUsage = Time.time + tiempoDeCadaColor;
+			gameObject.renderer.material.color = colors [Random.Range (0, colors.Length)];
+		}
+
 	}
 
-	void OnTriggerEnter(Collider other){
-		gameObject.renderer.material.color = colors [Random.Range (0, colors.Length)];
-	}
+
 }
