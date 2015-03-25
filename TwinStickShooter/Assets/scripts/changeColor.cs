00e8d9f70f80d8be79bea8030cabb89853b7130e
Sergio Roselló Morell <sergio-rosello@hotmail.com>
2015-03-25 13:06:29 +0100

    Retoque mapas
    Se han retocado un poco los Mapas, pero aun queda mucho por hacer.

---
+++ TwinStickShooter/Assets/scripts/changeColor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeColor : MonoBehaviour {
+
+	float nextUsage;
+	public Color[] colors;
+	public float tiempoDeCadaColor;
+	// Use this for initialization
+	void Start () {
+		nextUsage = Time.time + tiempoDeCadaColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Time.time > nextUsage)
+		{
+			nextUsage = Time.time + tiempoDeCadaColor;
+			gameObject.renderer.material.color = colors [Random.Range (0, colors.Length)];
+		}
+
+	}
+
+
+}
