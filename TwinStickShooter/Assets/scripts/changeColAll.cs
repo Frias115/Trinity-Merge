650900b93f3e833d967092d067af6ce2d405c5e7
Sergio Roselló Morell <sergio-rosello@hotmail.com>
2015-04-12 14:13:43 +0200

    Map 4

--- TwinStickShooter/Assets/scripts/changeColAll.cs
+++ TwinStickShooter/Assets/scripts/changeColAll.cs
@@ -6,6 +6,7 @@ using System.Collections;
 public class changeColAll : MonoBehaviour {
 	
 	float nextUsage;
+	float firstUsage;
 	public Color color1;
 	public Color color2;
 	public float lerpTime;
@@ -14,18 +15,26 @@ public class changeColAll : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		nextUsage = Time.time + tiempoDeCadaColor;
+		firstUsage = nextUsage / 2;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-		if (Time.time > nextUsage) {
-			nextUsage = Time.time + tiempoDeCadaColor;
+//		if (Time.time > nextUsage) {
+//			nextUsage = Time.time + tiempoDeCadaColor;
+//			gameObject.renderer.material.color = Color.Lerp (color1, color2, lerpTime);
+//		} else {nextUsage = Time.time + tiempoDeCadaColor;
+//			gameObject.renderer.material.color = Color.Lerp (color2, color1, lerpTime);}
+
+		if (Time.time > firstUsage) {
 			gameObject.renderer.material.color = Color.Lerp (color1, color2, lerpTime);
-		} else {nextUsage = Time.time + tiempoDeCadaColor;
+		} else if (Time.time > nextUsage){
+			nextUsage = Time.time + tiempoDeCadaColor;
+			firstUsage = nextUsage/2;
 			gameObject.renderer.material.color = Color.Lerp (color2, color1, lerpTime);}
-		
+		}
 	}
 	
 	
-}
+//}
