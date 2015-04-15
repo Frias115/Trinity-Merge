e0b34440c26f3fcf940e6e0a8f12e8b1f3b4f746
Roberto Frías Bernal <roberfrias115@gmail.com>
2015-04-15 16:45:13 +0200

    fix

--- TwinStickShooter/Assets/scripts/SpawnController.cs
+++ TwinStickShooter/Assets/scripts/SpawnController.cs
@@ -5,14 +5,29 @@ public class SpawnController : MonoBehaviour {
 	
 	public Transform[] spawnPoint;
 	public GameObject Enemy;
+	public static int enemigosRestantes = 0;
+	
 	// Use this for initialization
 	void Start () {
 		foreach (Transform child in spawnPoint) {
-			Instantiate(Enemy, child.position,child.rotation);
+			Instantiate (Enemy, child.position, child.rotation);
+			enemigosRestantes++;
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		/*
+		if (enemigosRestantes ==  0) {
+			spawnTimer +=  Time.deltaTime;
+			if(spawnTimer > waitTime){
+				foreach (Transform child in spawnPoint) {
+					Instantiate (Enemy, child.position, child.rotation);
+					enemigosRestantes++;
+				}
+				spawnTimer = 0;
+			}
+		}
+		*/
 	}
 }
